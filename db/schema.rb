7b15e31c398f0209d2eff2dada0d6768b4252249
Ubuntu <ubuntu@ip-172-31-29-16.us-east-2.compute.internal>
2021-07-04 05:42:35 +0000

    delete announcement

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_061706) do
-
-  create_table "announcements", force: :cascade do |t|
-    t.text "content"
-    t.string "seen"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.date "expiration"
-  end
+ActiveRecord::Schema.define(version: 2021_07_04_053946) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
